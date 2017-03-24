77cf33d02fb35a75c9367eb8b51295a4830a53cc
JC Franco <jfranco@esri.com>
2017-03-24 16:44:39 -0700

    Add innerHTML snippet.

---
+++ examples/InnerHTML.tsx
@@ -0,0 +1,33 @@
+/// <amd-dependency path="esri/core/tsSupport/declareExtendsHelper" name="__extends" />
+/// <amd-dependency path="esri/core/tsSupport/decorateHelper" name="__decorate" />
+
+import {subclass, declared} from "esri/core/accessorSupport/decorators";
+
+import Widget = require("esri/widgets/Widget");
+
+import {jsxFactory} from "esri/widgets/support/widget";
+
+const CSS = {
+  base: "esri-innerhtml"
+};
+
+@subclass("examples.InnerHTML")
+class InnerHTML extends declared(Widget) {
+
+  //--------------------------------------------------------------------------
+  //
+  //  Public Methods
+  //
+  //--------------------------------------------------------------------------
+
+  render() {
+    const someExternalContent = "<b>I am bold!</b>";
+
+    return (
+      <div class={CSS.base} innerHTML={someExternalContent} />
+    );
+  }
+
+}
+
+export = InnerHTML;
